a44e42342d76c5de5907fbd3e45edaac58bf1e0f
Fideris <fsus8677@uni.sydney.edu.au>
2024-08-24 15:30:32 +1000

    made changes to landing, route and search

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("landing.html")
+
+# Route for search page
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+# Route for About page
+@app.route('/route')
+def route():
+    return render_template('route.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
